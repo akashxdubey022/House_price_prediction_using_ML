f843c09851dbd7f3776897b98bf8a4e548a09ee9
akashdubey <dubeyakash22012000@gmail.com>
2023-05-21 11:39:23 +0530

    updated readme file and created route file.

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # House_price_prediction_using_ML
 I have written a Jupyter notebook on how a linear regression algorithm of machine learning can predict relational values Here I have predicted prices of house from Boston I have Boston dataset from sklearn.
+
+### Tools requried :
+1. [Github](https://github.com)
+2. [VScode IDE](https://code.visualstudio.com)
+3. [Git](https://git-scm.com/downloads)
---
+++ main.py
@@ -0,0 +1,31 @@
+from flask import Flask , render_template , request , app , jsonify , url_for
+import pickle 
+import numpy as np
+import pandas as pd
+
+
+app = Flask(__name__)
+model = pickle.load(open('model.pkl','rb'))
+scaler = pickle.load(open('scaler.pkl','rb'))
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/predict',methods =['POST'])
+def predict_api():
+    data=request.json['data']
+    print(data)
+
+    print(np.array(list(data.values())).reshape(1,-1))
+    new_data = scaler.transform(np.array(list(data.values())).reshape(1,-1))
+
+    output = model.predict(new_data)
+
+    print(output[0])
+    
+    return jsonify(output[0])
+    
+
+if __name__ == "__main__":
+    app.run(debug =True)
